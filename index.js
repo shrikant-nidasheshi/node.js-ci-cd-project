54147e2bce905e2ec21115623c0d984efaefb977
shrikant-nidasheshi <shreekantnidasheshi@gmail.com>
2026-03-31 23:57:17 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 8080;
+
+// Define a route for GET requests to the root URL
+app.get('/', (req, res) => {
+  res.send('Hello World from Express!');
+});
+
+// Start the server
+app.listen(4000, () => {
+  console.log(`Example app listening at `);
+});
