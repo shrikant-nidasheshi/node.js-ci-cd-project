04e060b2fc4b420e2a4e213a5a8e3f3f8c399425
shrikant-nidasheshi <shreekantnidasheshi@gmail.com>
2026-04-01 18:28:16 +0530

    app is updating

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const PORT = 3000; // or use 8080 if you prefer
 
 // Route
 app.get('/', (req, res) => {
-  res.send('Hello World from Express! shrikant amaresh nidasheshi');
+  res.send('Hello World from Express! shrikant amaresh nidasheshi kobihal');
 });
 
 // Start server
